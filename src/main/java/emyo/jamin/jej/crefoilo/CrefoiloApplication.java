b2b3e5b5c009c8cd10651b0349b8518d188ee844
jamin12 <rudals9901@naver.com>
2022-10-05 01:45:17 +0900

    first commit

---
+++ src/main/java/emyo/jamin/jej/crefoilo/CrefoiloApplication.java
@@ -0,0 +1,13 @@
+package emyo.jamin.jej.crefoilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrefoiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrefoiloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/emyo/jamin/jej/crefoilo/CrefoiloApplicationTests.java
@@ -0,0 +1,13 @@
+package emyo.jamin.jej.crefoilo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrefoiloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
