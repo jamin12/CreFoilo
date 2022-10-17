d533c4616c1c740b2b504bd4e20faa962f284f2e
jamin12 <rudals9901@naver.com>
2022-10-17 11:56:56 +0900

    Feat: portfolio 수정

--- src/main/java/emyo/jamin/jej/crefoilo/entity/Portfolio.java
+++ src/main/java/emyo/jamin/jej/crefoilo/entity/Portfolio.java
@@ -18,7 +18,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "portfolio")
-public class Portfoilo extends BaseTimeEntity {
+public class Portfolio extends BaseTimeEntity {
 
     @Id
     @Column(name = "portfolio_id", nullable = false)
@@ -28,12 +28,18 @@ public class Portfoilo extends BaseTimeEntity {
     @Column(name = "user_id", nullable = false, length = 255)
     private String userId; // '회원 ID',
 
-    @Column(name = "portfoilo_home_type", nullable = false)
-    private Integer portfoiloHomeType; // '홈 화면 type',
+    @Column(name = "portfolio_name", length = 45, nullable = false)
+    private String portfolioName; // '포트폴리오 이름
 
-    @Column(name = "portfoilo_home_color", nullable = false, length = 124)
-    private String portfoiloHomeColor; // '홈 화면 color',
+    @Column(name = "portfolio_home_type", nullable = false)
+    private Integer portfolioHomeType; // '홈 화면 type',
 
-    @Column(name = "portfoilo_home_text", nullable = false, length = 124)
-    private String portfoiloHomeText; // '홈화면 text',
+    @Column(name = "portfolio_home_color", nullable = false, length = 124)
+    private String portfolioHomeColor; // '홈 화면 color',
+
+    @Column(name = "portfolio_home_text", nullable = false, length = 124)
+    private String portfolioHomeText; // '홈화면 text',
+
+    @Column(name = "portfolio_home_img", nullable = true, length = 255)
+    private String portfolioHomeImg; // '홈화면 text',
 }
