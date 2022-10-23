aac7d1a0aba6bc8a2885d5d9de382481fa1bc425
jamin12 <rudals9901@naver.com>
2022-10-24 01:10:09 +0900

    Feat: other skill page
    1. 메서드 생성

---
+++ src/main/java/emyo/jamin/jej/crefoilo/service/OtherSkillService.java
@@ -0,0 +1,9 @@
+package emyo.jamin.jej.crefoilo.service;
+
+import java.util.List;
+
+import emyo.jamin.jej.crefoilo.entity.OtherSkill;
+
+public interface OtherSkillService {
+    List<OtherSkill> findOtherSkillList(Long portFolioId);
+}
---
+++ src/main/java/emyo/jamin/jej/crefoilo/service/OtherSkillServiceImpl.java
@@ -0,0 +1,24 @@
+package emyo.jamin.jej.crefoilo.service;
+
+import java.util.List;
+
+import emyo.jamin.jej.crefoilo.entity.OtherSkill;
+
+/**
+ * @author 강경민
+ */
+public class OtherSkillServiceImpl implements OtherSkillService {
+
+    /**
+     * 포트폴리에오 맞는 OtherSKill 리스트 조회
+     * 
+     * @param portFolioId 포트폴리오 아이디
+     * @return List<OtherSkill>
+     */
+    @Override
+    public List<OtherSkill> findOtherSkillList(Long portFolioId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
