2b155d42546399610c792b2a0e3b2f9c8d592d67
minjinkang <alswls0077@gmail.com>
2022-10-25 17:23:35 +0900

    feat view-page
    - 포트폴리오리스트조회 기능구현

---
+++ src/main/java/emyo/jamin/jej/crefoilo/controller/PortfolioListController.java
@@ -0,0 +1,24 @@
+package emyo.jamin.jej.crefoilo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import emyo.jamin.jej.crefoilo.repository.dslmodel.PortfolioDsllmpl;
+import emyo.jamin.jej.crefoilo.service.PortfolioListService;
+
+@Controller
+public class PortfolioListController {
+    
+    @Autowired
+    private PortfolioListService portfolioListService;
+
+    @GetMapping(value = "/hello")
+    public String portfolio() {
+        portfolioListService.findPortfolioList();
+        return "login";
+    }
+    
+}
---
+++ src/main/java/emyo/jamin/jej/crefoilo/service/PortfolioListService.java
@@ -0,0 +1,5 @@
+package emyo.jamin.jej.crefoilo.service;
+
+public interface PortfolioListService {
+    void findPortfolioList();
+}
