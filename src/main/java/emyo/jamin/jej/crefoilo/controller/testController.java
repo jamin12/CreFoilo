9c5cc9476ae58f05553abf9619286ef8a3d1141d
jamin12 <rudals9901@naver.com>
2022-11-02 22:23:35 +0900

    Feat: setting page
    1. 필요없는 데이터베이스 삭제

--- src/main/java/emyo/jamin/jej/crefoilo/controller/testController.java
+++ src/main/java/emyo/jamin/jej/crefoilo/controller/testController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import emyo.jamin.jej.crefoilo.dto.OtherSkillListDto;
+import emyo.jamin.jej.crefoilo.dto.ProjectDto;
 import emyo.jamin.jej.crefoilo.service.OtherSkillService;
+import emyo.jamin.jej.crefoilo.service.ProjectService;
 
 @RestController
 public class testController {
@@ -15,8 +17,11 @@ public class testController {
     @Autowired
     private OtherSkillService otherSkillService;
 
+    @Autowired
+    private ProjectService projectService;
+
     @GetMapping(value = "/test")
-    public List<OtherSkillListDto> test() {
-        return otherSkillService.findOtherSkillList(1L);
+    public List<ProjectDto> test() {
+        return projectService.findProjectList(3L, "100625979022689944834");
     }
 }
