44385bdc8b8e921fb4b340cfad175a38d0347209
jamin12 <rudals9901@naver.com>
2022-11-08 01:52:15 +0900

    Feat: setting page
    1. home view

--- src/main/java/emyo/jamin/jej/crefoilo/controller/testController.java
+++ src/main/java/emyo/jamin/jej/crefoilo/controller/testController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import emyo.jamin.jej.crefoilo.dto.ContactDto;
+import emyo.jamin.jej.crefoilo.dto.HomeViewDto;
 import emyo.jamin.jej.crefoilo.dto.OtherSkillListDto;
 import emyo.jamin.jej.crefoilo.dto.ProjectDetailDto;
 import emyo.jamin.jej.crefoilo.dto.ProjectDto;
 import emyo.jamin.jej.crefoilo.repository.ProejectRepository;
 import emyo.jamin.jej.crefoilo.service.ContactService;
 import emyo.jamin.jej.crefoilo.service.OtherSkillService;
+import emyo.jamin.jej.crefoilo.service.PortfolioService;
 import emyo.jamin.jej.crefoilo.service.ProjectService;
 
 @RestController
@@ -29,6 +31,9 @@ public class testController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private PortfolioService portfolioService;
+
     @GetMapping(value = "/test")
     public ProjectDetailDto test() {
         return projectService.findProjectDetail(1L, "100625979022689944834");
@@ -39,5 +44,10 @@ public class testController {
     public List<ContactDto> test1() {
         return contactService.findContact(1L);
         // return projectService.findProjectList(3L, "100625979022689944834");
+    }
+
+    @GetMapping(value = "/test2")
+    public HomeViewDto test2() {
+        return portfolioService.findPortfolioHome(1L);
     }
 }
