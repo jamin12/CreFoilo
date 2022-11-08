856684aaa405e853340f070411604396514a406e
jamin12 <rudals9901@naver.com>
2022-11-09 00:44:48 +0900

    Feat: setting page
    1. contact update create select 작업

--- src/main/java/emyo/jamin/jej/crefoilo/controller/testController.java
+++ src/main/java/emyo/jamin/jej/crefoilo/controller/testController.java
@@ -1,5 +1,6 @@
 package emyo.jamin.jej.crefoilo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import emyo.jamin.jej.crefoilo.dto.ContactDto;
+import emyo.jamin.jej.crefoilo.dto.FindLanguageDto;
 import emyo.jamin.jej.crefoilo.dto.HomeViewDto;
 import emyo.jamin.jej.crefoilo.dto.OtherSkillListDto;
 import emyo.jamin.jej.crefoilo.dto.ProjectDetailDto;
 import emyo.jamin.jej.crefoilo.dto.ProjectDto;
 import emyo.jamin.jej.crefoilo.repository.ProejectRepository;
 import emyo.jamin.jej.crefoilo.service.ContactService;
+import emyo.jamin.jej.crefoilo.service.LanguageService;
 import emyo.jamin.jej.crefoilo.service.OtherSkillService;
 import emyo.jamin.jej.crefoilo.service.PortfolioService;
 import emyo.jamin.jej.crefoilo.service.ProjectService;
@@ -34,6 +37,9 @@ public class testController {
     @Autowired
     private PortfolioService portfolioService;
 
+    @Autowired
+    private LanguageService languageService;
+
     @GetMapping(value = "/test")
     public ProjectDetailDto test() {
         return projectService.findProjectDetail(1L, "100625979022689944834");
@@ -49,5 +55,20 @@ public class testController {
     @GetMapping(value = "/test2")
     public HomeViewDto test2() {
         return portfolioService.findPortfolioHome(1L);
+    }
+
+    @GetMapping(value = "/test3")
+    public List<FindLanguageDto> test3() {
+        return languageService.findLanguage(1L);
+    }
+
+    @GetMapping(value = "/test4")
+    public String test4() {
+        List<ContactDto> contacts = new ArrayList<ContactDto>();
+        contacts.add(new ContactDto(1L, "phone", "새로새로"));
+        contacts.add(new ContactDto(1L, "git", "새로 만드렁써 되써용"));
+        contacts.add(new ContactDto(1L, "myimg", "새로아하하"));
+        contacts.add(new ContactDto(1L, "phone", "이건 새애애애롱"));
+        return contactService.createAndUpdateContact(1L, "100625979022689944834", contacts);
     }
 }
--- src/main/java/emyo/jamin/jej/crefoilo/service/ContactServiceImpl.java
+++ src/main/java/emyo/jamin/jej/crefoilo/service/ContactServiceImpl.java
@@ -9,12 +9,16 @@ import org.springframework.stereotype.Service;
 import emyo.jamin.jej.crefoilo.dto.ContactDto;
 import emyo.jamin.jej.crefoilo.entity.Contact;
 import emyo.jamin.jej.crefoilo.repository.ContactRepository;
+import emyo.jamin.jej.crefoilo.utils.Validation;
 
 @Service
 public class ContactServiceImpl implements ContactService {
 
     @Autowired
     private ContactRepository contactRepository;
+
+    @Autowired
+    private Validation validation;
 
     /**
      * 아래 코드는 view page 작업입니다.
@@ -32,6 +36,49 @@ public class ContactServiceImpl implements ContactService {
             contactDtoList.add(new ContactDto(contact));
         });
         return contactDtoList;
+    }
+
+    /**
+     * 아래 코드는 setting page 작업입니다.
+     */
+    /**
+     * 컨택트 조회
+     * 
+     * @param portfolioId
+     * @param userId
+     */
+    @Override
+    public List<ContactDto> findContact(Long portfolioId, String userId) {
+        validation.checkUserHasPortfolio(portfolioId, userId);
+        List<Contact> findedContactList = contactRepository.findByPortfolioId(portfolioId);
+        List<ContactDto> contactDtoList = new ArrayList<>();
+        findedContactList.forEach((contact) -> {
+            contactDtoList.add(new ContactDto(contact));
+        });
+        return contactDtoList;
+    }
+
+    /**
+     * contact 생성
+     * 
+     * @param portfolioId
+     * @param userId
+     * @param contactDtoList
+     */
+    @Override
+    public String createAndUpdateContact(Long portfolioId, String userId, List<ContactDto> contactDtoList) {
+        List<Contact> findedContactList = contactRepository.findByPortfolioId(portfolioId);
+        contactDtoList.forEach((contactDto) -> {
+            findedContactList.forEach((findedContact) -> {
+                if (contactDto.getContactId() != null) {
+                    if (findedContact.getContactId() == contactDto.getContactId()) {
+
+                    }
+                }
+            });
+            contactRepository.save(Contact.createAndUpdateEntity(portfolioId, contactDto));
+        });
+        return "success";
     }
 
 }
