a602cb93daba67d57c1645fc295f6996d7711735
minjinkang <alswls0077@gmail.com>
2022-11-09 18:45:42 +0900

    Feat: setting page
    1. portfolio 삭제

--- src/main/java/emyo/jamin/jej/crefoilo/repository/PortfolioRepository.java
+++ src/main/java/emyo/jamin/jej/crefoilo/repository/PortfolioRepository.java
@@ -3,6 +3,7 @@ package emyo.jamin.jej.crefoilo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import emyo.jamin.jej.crefoilo.dto.PortfolioDto;
 import emyo.jamin.jej.crefoilo.entity.Portfolio;
 import emyo.jamin.jej.crefoilo.repository.dslmodel.PortfolioDsl;
 
