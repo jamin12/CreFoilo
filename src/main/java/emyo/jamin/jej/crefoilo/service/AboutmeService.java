d0e3a061620a218702635331b4ef40069497098a
jamin12 <rudals9901@naver.com>
2022-11-13 23:56:44 +0900

    style: setting page
    1. 필요없는 import제거

--- src/main/java/emyo/jamin/jej/crefoilo/service/AboutmeService.java
+++ src/main/java/emyo/jamin/jej/crefoilo/service/AboutmeService.java
@@ -1,7 +1,6 @@
 package emyo.jamin.jej.crefoilo.service;
 
 import emyo.jamin.jej.crefoilo.dto.AboutmeDto;
-import emyo.jamin.jej.crefoilo.entity.AboutMe;
 
 public interface AboutmeService {
     AboutmeDto findAboutme(Long portfolioId);
