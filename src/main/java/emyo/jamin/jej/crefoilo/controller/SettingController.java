e5b6d70723f1a01e243e578260817ab3a1358372
jamin12 <rudals9901@naver.com>
2022-11-29 22:43:21 +0900

    feat: project detail
    1. 프로젝트 디테일 수정 생성(하는중)

--- src/main/java/emyo/jamin/jej/crefoilo/controller/SettingController.java
+++ src/main/java/emyo/jamin/jej/crefoilo/controller/SettingController.java
@@ -94,7 +94,16 @@ public class SettingController {
     @ResponseBody
     @PostMapping(value = { "/setting/projectdetail", "/setting/projectdetail/{projectid}" })
     public String settingProjectDetailPost(@RequestBody ProjectDetailDto projectDetailDto,
-            @PathVariable Optional<Long> projectid) {
+            @PathVariable Optional<Long> projectid, Model model) {
+        SessionUser userIdInSession = (SessionUser) httpSession.getAttribute("user");
+        model.addAttribute("name", userIdInSession.getSnsName());
+        if (!projectid.isPresent()) {
+            projectService.createProject(projectDetailDto.getPortfolioId(), userIdInSession.getUserId(),
+                    projectDetailDto);
+            return "/setting/project/" + projectDetailDto.getPortfolioId().toString();
+        }
+        projectService.updateProject(projectid.get(), userIdInSession.getUserId(), projectDetailDto);
         return "/setting/project/" + projectDetailDto.getPortfolioId().toString();
     }
+
 }
