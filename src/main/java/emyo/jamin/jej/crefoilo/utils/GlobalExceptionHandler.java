61bfa763d8eb121f40841881ecf7fd29cd40a4c5
jamin12 <rudals9901@naver.com>
2022-12-28 23:49:05 +0900

    sytle: 필요없는 import제거

--- src/main/java/emyo/jamin/jej/crefoilo/utils/GlobalExceptionHandler.java
+++ src/main/java/emyo/jamin/jej/crefoilo/utils/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package emyo.jamin.jej.crefoilo.utils;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
